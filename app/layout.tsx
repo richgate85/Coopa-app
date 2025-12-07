e7bbc94dc08dd15151594178375e367f555ec95c
udoyen <datameshprojects@gmail.com>
2025-12-07 09:01:49 +0100

    Fixed hydration waring

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className=""> 
+      <body className="" suppressHydrationWarning={true}> 
         <AuthProvider>
           {children}
           <Analytics />
